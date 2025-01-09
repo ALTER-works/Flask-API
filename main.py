a47a127272075697f937e0655fd5ac087ea6a86f
ALTER-works <igor.sharlap@gmail.com>
2025-01-09 10:07:34 +0300

    Modified files: main.py, main_page.html. Now translation based on AccuWeather API. Added better visualisation for chosen cities.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
+from fnmatch import translate
+
 import requests
-from deep_translator import GoogleTranslator as GT
 from flask import Flask, render_template, request
+
+bad_weather_states = [
+    "Метель",
+    "Гроза",
+    "Снег",
+    "Туман"
+]
 
 app = Flask(__name__)
 
 api_key = "wGsQrNjdQkt44aWAuPAR8JYsgfNOoN0a"
-
-def translate(s, lng):
-    return GT(source='auto', target=f'{lng}').translate(s)
-
 
 def get_location_key(city):
     location_url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}"
@@ -23,10 +27,9 @@ def get_location_key(city):
             }
     return None
 
-
 def get_current_weather(location_key, location_key2):
-    weather_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={api_key}"
-    weather_url2 = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key2}?apikey={api_key}"
+    weather_url = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key}?apikey={api_key}&language=ru-ru"
+    weather_url2 = f"http://dataservice.accuweather.com/currentconditions/v1/{location_key2}?apikey={api_key}&language=ru-ru"
 
     weather_response = requests.get(weather_url)
     weather_response2 = requests.get(weather_url2)
@@ -34,7 +37,6 @@ def get_current_weather(location_key, location_key2):
     if weather_response.status_code == 200 and weather_response2.status_code == 200:
         return [weather_response.json()[0], weather_response2.json()[0]]  # Return both sets of data
     return None
-
 
 @app.route('/', methods=['GET', 'POST'])
 def main():
@@ -54,19 +56,14 @@ def main():
             all_info[1].update(
                 {'GeoPosition': {'Latitude': location_data2['Latitude'], 'Longitude': location_data2['Longitude']}})
 
-            all_info[0]['LocalizedName'] = translate(request.form['city'], 'ru')
-            if all_info[0]['WeatherText'] == 'Clear': all_info[0]['WeatherText'] = 'Ясно'
-            else: all_info[0]['WeatherText'] = translate(all_info[0]['WeatherText'], 'ru')
-
-            all_info[1]['LocalizedName'] = translate(request.form['city2'], 'ru')
-            if all_info[1]['WeatherText'] == 'Clear': all_info[1]['WeatherText'] = 'Ясно'
-            else: all_info[1]['WeatherText'] = translate(all_info[1]['WeatherText'], 'ru')
-
         else:
             all_info = {"error": "Не удалось найти местоположение."}
 
-    return render_template('main_page.html', weather_info=all_info)
-
+    return render_template(
+        'main_page.html',
+        weather_info=all_info,
+        bad_weather_states=bad_weather_states
+    )
 
 if __name__ == '__main__':
     app.run(debug = True)
